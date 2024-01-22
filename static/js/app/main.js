82f4e80afa41efd60378cdcaf7ede7972afd1b66
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2024-01-22 17:30:07 +0200

    Cleanup text

--- static/js/app/main.js
+++ static/js/app/main.js
@@ -121,7 +121,7 @@ var tableFormatters = {
             fetch(url)
               .then((response) => response.text())
               .then((text) => {
-                navigator.clipboard.writeText(text);
+                navigator.clipboard.writeText(text.slice(1, -1).replace('\"', '"'));
                 showNotify("SUCCESS", "Copied run command to clipboard")
               });
         },
